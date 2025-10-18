433e51362b57ee8cbbbefea2419d16b23cd85d03
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-18 13:16:09 +0000

    lower case

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase, followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+
+putchar(l);
+}
+return (0);
+}
