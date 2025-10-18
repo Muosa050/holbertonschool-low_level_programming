cdc7800d2296a6f79f8bb97f8f9dcf32fb75952a
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-18 13:22:51 +0000

    lower case & ubber case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase then ubber case, by a new line
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'Z'; l++)
+{
+
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
