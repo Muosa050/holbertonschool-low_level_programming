488a64120118542bdbef9bfc6f7f51713b9f5d5b
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-22 17:21:35 +0000

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+*main - fun to print fizz if divided by 3 buzz if it 5
+*
+*Return: void
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 1 ; i <= 100 ; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+putchar('\n');
+return (i);
+}
