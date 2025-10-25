11a9480de11d6275de9a7edad27505b98f4fa650
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-25 22:31:32 +0000

    prit revirce statment

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string to reverse
+ */
+void rev_string(char *s)
+{
+int start = 0;
+int end = 0;
+char temp;
+while (s[end] != '\0')
+{
+end++;
+}
+end--;
+while (start < end)
+{
+temp = s[start];
+s[start] = s[end];
+s[end] = temp;
+start++;
+end--;
+}
+}
