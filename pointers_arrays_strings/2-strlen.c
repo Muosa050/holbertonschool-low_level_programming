6058e6ff1ed5033583d7a9cbc3151f3fbd5b154d
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-25 22:14:25 +0000

    return the legnth

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - function that return the legnth of str
+*
+*@s: string to be calculated
+*
+*Return: a as a length of string s
+*/
+int _strlen(char *s)
+{
+int a = strlen(s);
+return (a);
+}
