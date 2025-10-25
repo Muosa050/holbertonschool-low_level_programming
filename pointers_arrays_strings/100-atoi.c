576f3d4140db8152fd2900fc7d57d996dc85989d
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-25 23:54:38 +0000

    str to no

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _atoi - convert string to integer
+ * _atoi - convert string to integer safely
  * @s: input string
  * Return: converted integer
  */
@@ -13,10 +13,24 @@ int started = 0;
 while (s[i] != '\0')
 {
 if (s[i] == '-')
-sign = sign * -1;
+sign *= -1;
 else if (s[i] >= '0' && s[i] <= '9')
 {
-result = result *10 + (s[i] - '0');
+if (result > 214748364)
+{
+if (sign == 1)
+return (2147483647);
+else
+return (-2147483648);
+}
+if (result == 214748364)
+{
+if (sign == 1 && (s[i] - '0') > 7)
+return (2147483647);
+if (sign == -1 && (s[i] - '0') > 8)
+return (-2147483648);
+}
+result = result * 10 + (s[i] - '0');
 started = 1;
 }
 else if (started == 1)
