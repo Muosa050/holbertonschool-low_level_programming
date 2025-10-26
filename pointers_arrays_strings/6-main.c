8202ab391bd764b1061a566f5ca00ed1e1b24d8e
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-26 03:32:17 +0000

    6

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -1,12 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check puts2
- * Return: 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-char *str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-puts2(str);
-return (0);
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
