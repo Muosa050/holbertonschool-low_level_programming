b79989ebaf5106a587c7e8dfd6251f5feb21edea
Muosa050 <mosa.alqarni01@gmail.com>
2025-10-29 16:17:06 +0000

    gg

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*set_string - put string into another place
+*
+*@s: pointer to the pointer of string
+*@to: destnation
+*
+*Return: void function
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
