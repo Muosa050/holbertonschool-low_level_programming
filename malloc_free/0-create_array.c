e5fd9cf600cdefdff1748c7955bf6b284a9760c2
Muosa050 <mosa.alqarni01@gmail.com>
2025-11-03 12:58:40 +0000

    memory allocate, get 0x value for c alphabet

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,11 +13,13 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int i;
 char *array;
+if (size == 0)
+return (NULL);
+
 array = malloc(size * sizeof(char));
 for (i = 0 ; i < size ; i++)
 array[i] = c;
-if ((size == 0) || (array == NULL))
-return ("null");
-free(array);
+if (array == NULL)
+return (NULL);
 return (array);
 }
