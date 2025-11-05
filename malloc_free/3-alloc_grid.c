8623ce6323923a23ca3662eee457d26d46378003
Muosa050 <mosa.alqarni01@gmail.com>
2025-11-05 15:38:42 +0000

    array in 2D in the memory

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -5,27 +5,27 @@
 *@height:height of the grid
 *Return:pointer to 2D array,or NULL on failure
 */
-int **alloc_grid(int width,int height)
+int **alloc_grid(int width, int height)
 {
 int **grid;
-int i,j;
-if(width<=0||height<=0)
-return(NULL);
-grid=malloc(sizeof(int *)*height);
-if(grid==NULL)
-return(NULL);
-for(i=0;i<height;i++)
+int i, j;
+if (width <= 0 || height <= 0)
+return (NULL);
+grid = malloc(sizeof(int *) * height);
+if (grid == NULL)
+return (NULL);
+for (i = 0 ; i < height ; i++)
 {
-grid[i]=malloc(sizeof(int)*width);
-if(grid[i]==NULL)
+grid[i] = malloc(sizeof(int) * width);
+if (grid[i] == NULL)
 {
-while(i--)
+while (i--)
 free(grid[i]);
 free(grid);
-return(NULL);
+return (NULL);
 }
-for(j=0;j<width;j++)
-grid[i][j]=0;
+for (j = 0 ; j < width ; j++)
+grid[i][j] = 0;
 }
-return(grid);
+return (grid);
 }
