5bd1ea4faf94ce24d44f98e393c2916506d8bf3c
Muosa050 <mosa.alqarni01@gmail.com>
2025-11-09 03:52:10 +0000

    do this ->

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,18 +13,13 @@
 void print_dog(struct dog *d)
 {
 if (d == NULL)
-printf("nil\n");
-
+return;
 if (d->name == NULL)
-{
 printf("Name: nil\n");
-printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner);
-}
-else
-{
+if (d->owner == NULL)
+printf("Owner: nil\n");
+
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
-}
 }
