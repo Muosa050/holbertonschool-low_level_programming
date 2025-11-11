65042f71460f8f5f76a2eec8423321a5a058bc6c
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-11-11 17:07:30 +0000

    str compairing, exit in order, 4 files worth to be read

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+*op_add - addation function
+*
+*@a: first oprand
+*@b: second oprand
+*
+*Return: value of opration between a & b
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+*op_sub - addation function
+*
+*@a: first oprand
+*@b: second oprand
+*
+*Return: value of opration between a & b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+*op_mul - addation function
+*
+*@a: first oprand
+*@b: second oprand
+*
+*Return: value of opration between a & b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+*op_div - addation function
+*
+*@a: first oprand
+*@b: second oprand
+*
+*Return: value of opration between a & b
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+*op_mod - addation function
+*
+*@a: first oprand
+*@b: second oprand
+*
+*Return: value of opration between a & b
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
+
+
