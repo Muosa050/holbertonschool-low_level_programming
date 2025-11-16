11403c0258bca127843e61ec6b2b1fcd3c3bf387
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-11-16 06:43:42 +0000

    va-.. list v, start 2 v & c, arg v t, end

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,58 +2,71 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/* Helper functions */
+/**
+ * print_char - prints a char
+ * @args: argument list
+ */
 void print_char(va_list args)
 {
 printf("%c", va_arg(args, int));
 }
 
+/**
+ * print_int - prints an integer
+ * @args: argument list
+ */
 void print_int(va_list args)
 {
 printf("%d", va_arg(args, int));
 }
 
+/**
+ * print_float - prints a float
+ * @args: argument list
+ */
 void print_float(va_list args)
 {
 printf("%f", (double)va_arg(args, double));
 }
 
+/**
+ * print_string - prints a string
+ * @args: argument list
+ */
 void print_string(va_list args)
 {
 char *s = va_arg(args, char *);
-if (s == NULL)
-{
-printf("(nil)");
-return;
+const char *map[2];
+
+map[0] = s;
+map[1] = "(nil)";
+printf("%s", map[s == NULL]);
 }
-printf("%s", s);
-}
+
 /**
  * print_all - prints anything
- * @format: list of types of arguments
- *
- * Return: SUCCESS
+ * @format: list of argument types
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
 int i = 0, j = 0, printed = 0;
 char types[] = "cifs";
-void (*funcs[])(va_list) = {
-print_char, print_int, print_float, print_string
-};
+void (*funcs[])(va_list) = {print_char, print_int, print_float, print_string};
+
 va_start(args, format);
-while (format && format[i])    /* 1st while */
+
+while (format && format[i])
 {
 j = 0;
-while (types[j])              /* 2nd while */
+
+while (types[j])
 {
-/* ---- 1st if ---- */
 if (format[i] == types[j])
 {
-/* ---- 2nd if ---- */
 if (printed)
 printf(", ");
+
 funcs[j](args);
 printed = 1;
 }
@@ -61,6 +74,7 @@ j++;
 }
 i++;
 }
+
 printf("\n");
 va_end(args);
 }
